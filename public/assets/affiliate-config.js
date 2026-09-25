375870b9fe0969800742d64371e4e276d6f22829
canonboyddd <super.canon.boy@gmail.com>
2026-09-25 21:11:16 +0900

    Record FANZA API affiliate ID

--- public/assets/affiliate-config.js
+++ public/assets/affiliate-config.js
@@ -1,6 +1,7 @@
 window.AFFILIATE_CONFIG = {
   approved: true,
   affiliateId: "okazumidori-001",
+  apiAffiliateId: "okazumidori-990",
   apiId: "",
   links: {
     video: "https://al.dmm.co.jp/?lurl=https%3A%2F%2Fwww.dmm.co.jp%2Fdigital%2Fvideoa%2F&af_id=okazumidori-001&ch=link_tool&ch_id=link",
@@ -8,5 +9,5 @@ window.AFFILIATE_CONFIG = {
     monthly: "https://al.dmm.co.jp/?lurl=https%3A%2F%2Fwww.dmm.co.jp%2Fmonthly%2F&af_id=okazumidori-001&ch=link_tool&ch_id=link",
     premium: "https://al.dmm.co.jp/?lurl=https%3A%2F%2Fpremium.dmm.co.jp%2F&af_id=okazumidori-001&ch=link_tool&ch_id=link"
   },
-  note: "DMMアフィリエイト審査承認済み。新形式アフィリエイトリンクを使用。"
+  note: "DMMアフィリエイト審査承認済み。FANZA Webサービス商品APIはGitHub ActionsのSecretから実行。"
 };
